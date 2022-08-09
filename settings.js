09b442f14d70d9ef3846053eb028efbcb0d53bb0
Kimzxd <110285634+Kimzxd@users.noreply.github.com>
2022-08-09 19:04:43 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,23 +24,22 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "KYYYBOTZ" //namabot kalian
-global.ownername= "KYYYGANTENG" //nama kalian
+global.botname = "KIMZXD-BOTZ" //namabot kalian
+global.ownername= "TOMZ《KIMZXD》" //nama kalian
 global.myweb ="https://chat.whatsapp.com/J4LIfG5WjzqARFVniPUWu9" //bebas asal jan hapus
 global.youtube = "https://chat.whatsapp.com/J4LIfG5WjzqARFVniPUWu9" //bebas asal jan hapus
 global.github = "https://chat.whatsapp.com/J4LIfG5WjzqARFVniPUWu9" //bebas
 global.email = "tomzkimzxd86@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6285806583988'] //premium user
-global.owner = ["6285806583988","6285811395565","085692219286"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "085811395565" // nomor wa kalian
+global.premium = ["6285806583988"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285806583988" // nomor wa kalian
 global.ownernomerr = "+6285806583988" //nmr wa kalian
-global.thumbnail = "./image/IMG-20220731-WA0015.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/IMG-20220731-WA0014.jpg" // foto donasi di folder image
+global.thumbnail = "./image/IMG-20220805-WA0000.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/IMG-20220724-WA0197.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = 'Botz KYYYBOTZ' //sticker wm ubah
-global.author = 'Di Buat Oleh KYYYGANTENG' //sticker wm ganti nama kalian
+global.packname = 'Botz KIMZXD-BOTZ' //sticker wm ubah
+global.author = 'Di Buat Oleh TOMZ[KIMZXD]' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
